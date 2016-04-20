9a5026f96b80f62173b1fcd06060b469409b52e7
pastak <pasta0915@gmail.com>
2016-04-20 13:57:28 +0900

    仕様変わってたので追随

--- 03/BubbleSort.java
+++ 03/BubbleSort.java
@@ -1,19 +1,18 @@
 public class BubbleSort {
-  public int[] sort(int[] input) {
-    int unsorted = input.length;
+  public static void sort(int[] array) {
+    int unsorted = array.length;
     int i, tmp;
     while (unsorted > 2) {
       i = 0;
       while (i+1 < unsorted) {
-        if (input[i] > input[i+1]) {
-          tmp = input[i];
-          input[i] = input[i+1];
-          input[i+1] = tmp;
+        if (array[i] > array[i+1]) {
+          tmp = array[i];
+          array[i] = array[i+1];
+          array[i+1] = tmp;
         }
         i++;
       }
       unsorted--;
     }
-    return input;
   }
 }
--- 03/SortTest.java
+++ 03/SortTest.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class SortTest {
-  public static int[] read() {
+  public static int[] input() {
     // 標準入力からの入力ファイル名を受け取り，入力ファイル内のソート対象データを配列に格納し，得られた配列を返す．
     Scanner scan = new Scanner(System.in);
     System.out.print("Type your input file name: ");
@@ -19,6 +19,14 @@ public class SortTest {
       System.out.println("Filename: " + intputFileName + " is not found");
       return null;
     }
+    try {
+      return validate(numbers);
+    } catch (IllegalStateException ex) {
+      System.out.println("入力ファイルの内容が不正です: "+ex.getMessage());
+      return null;
+    }
+  }
+  public static int[] validate (ArrayList<Integer> numbers) {
     if (numbers.size() > 50) {
       throw new IllegalStateException("読み込むことが出来る値の個数は50個以下です");
     }
@@ -30,15 +38,15 @@ public class SortTest {
     }
     return res;
   }
-  public static void output(int[] sorted) {
+  public static void output(int[] array) {
     // ソートされたデータを画面に出力する．
     int counter = 1;
-    for (int num : sorted) {
+    for (int num : array) {
       if (num == 0) continue;
       System.out.print(rightJustify(num));
       if (counter % 10 == 0) {
         System.out.print("\n");
-      } else if (counter != sorted.length) {
+      } else if (counter != array.length) {
         System.out.print(" ");
       }
       counter++;
@@ -55,9 +63,8 @@ public class SortTest {
   }
 
   public static void main(String[] args) {
-    BubbleSort bsort = new BubbleSort();
-    int[] input = read();
-    int[] sorted = bsort.sort(input);
-    output(sorted);
+    int[] array = input();
+    BubbleSort.sort(array);
+    output(array);
   }
 }
