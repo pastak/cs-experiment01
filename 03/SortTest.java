d4cc644952827d67094cc76ff36228d3228be8ff
pastak <pasta0915@gmail.com>
2016-04-20 13:33:53 +0900

    ArrayListを使って読み込む。適当に例外を投げる

--- 03/SortTest.java
+++ 03/SortTest.java
@@ -2,28 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class SortTest {
-  public static int inputNumberCount;
-
   public static int[] read() {
     // 標準入力からの入力ファイル名を受け取り，入力ファイル内のソート対象データを配列に格納し，得られた配列を返す．
     Scanner scan = new Scanner(System.in);
     System.out.print("Type your input file name: ");
     String intputFileName = scan.next();
-    int[] numbers = new int [50];
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
     try {
       File inputFile = new File(intputFileName);
       Scanner sc = new Scanner(inputFile);
-      int i = 0;
       while (sc.hasNext()) {
         int num = sc.nextInt();
-        numbers[i++] = num;
+        numbers.add(num);
       }
-      inputNumberCount = i;
     } catch (FileNotFoundException ex) {
-      System.out.println("Filename: " + intputFileName + "is not found");
+      System.out.println("Filename: " + intputFileName + " is not found");
       return null;
     }
-    return numbers;
+    if (numbers.size() > 50) {
+      throw new IllegalStateException("読み込むことが出来る値の個数は50個以下です");
+    }
+    int[] res = new int[numbers.size()];
+    for (int i = 0; i < numbers.size(); i++) {
+      if (res[i] < 1 || res[i] > 9999) throw new IllegalStateException("入力可能な値は自然数で1以上9999以下です");
+      res[i] = numbers.get(i);
+    }
+    return res;
   }
   public static void output(int[] sorted) {
     // ソートされたデータを画面に出力する．
@@ -33,7 +37,7 @@ public class SortTest {
       System.out.print(rightJustify(num));
       if (counter % 10 == 0) {
         System.out.print("\n");
-      } else if (counter != inputNumberCount){
+      } else if (counter != sorted.length) {
         System.out.print(" ");
       }
       counter++;
