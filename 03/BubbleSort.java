bda5fca7ade921e4431ca654c05f33378b7c7179
pastak <pasta0915@gmail.com>
2016-04-20 14:03:16 +0900

    データが2つの時ソートできないバグあった

--- 03/BubbleSort.java
+++ 03/BubbleSort.java
@@ -2,7 +2,7 @@ public class BubbleSort {
   public static void sort(int[] array) {
     int unsorted = array.length;
     int i, tmp;
-    while (unsorted > 2) {
+    while (unsorted >= 2) {
       i = 0;
       while (i+1 < unsorted) {
         if (array[i] > array[i+1]) {
