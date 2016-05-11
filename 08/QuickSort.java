6ae8e3f55839ddb1cac2e6ef73281f30e781ac6d
pastak <pasta0915@gmail.com>
2016-05-11 15:04:22 +0900

    ピボットに適当な値を取る

--- 08/QuickSort.java
+++ 08/QuickSort.java
@@ -3,8 +3,22 @@ import java.io.*;
 
 public class QuickSort {
   public static int choosePivot(int[] array, int i, int j) {
-    // とりあえず何も考えずに先頭を返す
-    return i;
+    // 中央値っぽいのをピボットにする
+    int[] res = new int[2]; // [value, 中央との近さ]
+    res[1] = j - i;
+    int p = i; int center = (j - i) / 2;
+    // 小さい値が半分くらいあればそれをピボットということにする
+    while (p < j) {
+      int t = array[p];
+      int c = 0, l = 0;
+      for (l = i; t < array[l]; c++) l++;
+      if (res[1] > Math.abs(center - c)) {
+        res[0] = array[l];
+        res[1] = Math.abs(center - c);
+      }
+      p++;
+    }
+    return res[0];
   }
 
   private static void qsort(int[] array, int startPosition, int lastPosition) {
@@ -12,7 +26,7 @@ public class QuickSort {
       int i = startPosition;
       int j = lastPosition;
       // ピボットを適当に選ぶ
-      int pivot = array[choosePivot(array, i, j)];
+      int pivot = choosePivot(array, i, j);
       int tmp = 0;
       while (true) {
         // ピボットより大きい値を先頭より順に探す
